bbce719ee4281ffedd4e7e877bc00f884d159027
LBlaque <luke.black31@gmail.com>
2019-11-25 14:41:14 -0700

    Group Project Initial Commit

---
+++ pageObjects/groupPage.js
@@ -0,0 +1,31 @@
+module.exports = {
+    url: 'https://www.16personalities.com/',
+    commands: [],
+    elements: {
+        agreeMax: {
+            selector: ('//div[@class="option agree max"]')[0],
+            locateStrategy: 'xpath'
+        },
+        agreeMax2: {
+            selector: ('//div[@class="option agree max"]')[1],
+            locateStrategy: 'xpath'
+        },
+        agreeMax3: {
+            selector: ('//div[@class="option agree max"]')[2],
+            locateStrategy: 'xpath'
+        },
+
+        agreeMax4: {
+            selector: ('//div[@class="option agree max"]')[3],
+            locateStrategy: 'xpath'
+        },
+        agreeMax5: {
+            selector: ('//div[@class="option agree max"]')[4],
+            locateStrategy: 'xpath'
+        },
+        agreeMax6: {
+            selector: ('//div[@class="option agree max"]')[5],
+            locateStrategy: 'xpath'
+        },
+    }
+}
