c59f2ea96156286b601bb26f30468fda3f9cdf64
LBlaque <luke.black31@gmail.com>
2019-11-25 16:15:50 -0700

    groupPage updated

--- pageObjects/groupPage.js
+++ pageObjects/groupPage.js
@@ -27,5 +27,171 @@ module.exports = {
             selector: ('//div[@class="option agree max"]')[5],
             locateStrategy: 'xpath'
         },
+        agreeMed1: {
+            selector: ('//div[@class="option agree med"]')[0],
+            locateStrategy: 'xpath'
+        },
+        agreeMed2: {
+            selector: ('//div[@class="option agree med"]')[1],
+            locateStrategy: 'xpath'
+        },
+        agreeMed3: {
+            selector: ('//div[@class="option agree med"]')[2],
+            locateStrategy: 'xpath'
+        },
+        agreeMed4: {
+            selector: ('//div[@class="option agree med"]')[3],
+            locateStrategy: 'xpath'
+        },
+        agreeMed5: {
+            selector: ('//div[@class="option agree med"]')[4],
+            locateStrategy: 'xpath'
+        },
+        agreeMed6: {
+            selector: ('//div[@class="option agree med"]')[5],
+            locateStrategy: 'xpath'
+        },
+        agreeMin1: {
+            selector: ('//div[@class="option agree min"]')[0],
+            locateStrategy: 'xpath'
+        },
+        agreeMin2: {
+            selector: ('//div[@class="option agree min"]')[1],
+            locateStrategy: 'xpath'
+        },
+        agreeMin3: {
+            selector: ('//div[@class="option agree min"]')[2],
+            locateStrategy: 'xpath'
+        },
+        agreeMin4: {
+            selector: ('//div[@class="option agree min"]')[3],
+            locateStrategy: 'xpath'
+        },
+        agreeMin5: {
+            selector: ('//div[@class="option agree min"]')[4],
+            locateStrategy: 'xpath'
+        },
+        agreeMin6: {
+            selector: ('//div[@class="option agree min"]')[5],
+            locateStrategy: 'xpath'
+        },
+        neutral1: {
+            selector: ('//div[@class="option neutral"]')[0],
+            locateStrategy: 'xpath'
+        },
+        neutral2: {
+            selector: ('//div[@class="option neutral"]')[1],
+            locateStrategy: 'xpath'
+        },
+        neutral3: {
+            selector: ('//div[@class="option neutral"]')[2],
+            locateStrategy: 'xpath'
+        },
+        neutral4: {
+            selector: ('//div[@class="option neutral"]')[3],
+            locateStrategy: 'xpath'
+        },
+        neutral5: {
+            selector: ('//div[@class="option neutral"]')[4],
+            locateStrategy: 'xpath'
+        },
+        neutral6: {
+            selector: ('//div[@class="option neutral"]')[5],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin1: {
+            selector: ('//div[@class="option disagree min"]')[0],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin2: {
+            selector: ('//div[@class="option disagree min"]')[1],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin3: {
+            selector: ('//div[@class="option disagree min"]')[2],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin4: {
+            selector: ('//div[@class="option disagree min"]')[3],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin5: {
+            selector: ('//div[@class="option disagree min"]')[4],
+            locateStrategy: 'xpath'
+        },
+        disagreeMin6: {
+            selector: ('//div[@class="option disagree min"]')[5],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed1: {
+            selector: ('//div[@class="option disagree med"]')[0],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed2: {
+            selector: ('//div[@class="option disagree med"]')[1],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed3: {
+            selector: ('//div[@class="option disagree med"]')[2],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed4: {
+            selector: ('//div[@class="option disagree med"]')[3],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed5: {
+            selector: ('//div[@class="option disagree med"]')[4],
+            locateStrategy: 'xpath'
+        },
+        disagreeMed6: {
+            selector: ('//div[@class="option disagree med"]')[5],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax1: {
+            selector: ('//div[@class="option disagree max"]')[0],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax2: {
+            selector: ('//div[@class="option disagree max"]')[1],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax3: {
+            selector: ('//div[@class="option disagree max"]')[2],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax4: {
+            selector: ('//div[@class="option disagree max"]')[3],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax5: {
+            selector: ('//div[@class="option disagree max"]')[4],
+            locateStrategy: 'xpath'
+        },
+        disagreeMax6: {
+            selector: ('//div[@class="option disagree max"]')[5],
+            locateStrategy: 'xpath'
+        },
+        nextButton: 'button[type="button"]',
+        takeTheTest: 'a[class="btn btn-action"]',
+        testResult: 'div[class="type-name"]',
+        personalitiesButton: 'img[alt="16Personalities"]',
+        youMayKnow: 'span[class="fal fa-angle-right"]',
+        nextArrow: 'div[class="fal fa-long-arrow-right"]',
+        architect: 'svg[class="architect"]',
+        logician: 'svg[class="logician"]',
+        commander: 'svg[class="commander"]',
+        debater: 'svg[class="debater"]',
+        advocate: 'svg[class="advocate"]',
+        mediator: 'svg[class="mediator"]',
+        protagonist: 'svg[class="protagonist"]',
+        campaigner: 'svg[class="campaigner"]',
+        logician: 'svg[class="logician"]',
+        defender: 'svg[class="defender"]',
+        executive: 'svg[class="executive"]',
+        consul: 'svg[class="consul"]',
+        virtuoso: 'svg[class="virtuoso"]',
+        adventurer: 'svg[class="adventurer"]',
+        entrepeneur: 'svg[class="entrepeneur"]',
+        entertainer: 'svg[class="entertainer"]',
     }
 }
