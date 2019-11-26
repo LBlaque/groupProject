2e330d39c47c7595577529fded8084b0537b7716
LBlaque <luke.black31@gmail.com>
2019-11-26 16:52:31 -0700

    good selectors

--- tests/groupTest.js
+++ tests/groupTest.js
@@ -3,14 +3,44 @@ module.exports = {
     beforeEach: browser => {
         personalities = browser.page.groupPage()
         personalities
-        .navigate()
+            .navigate()
     },
     after: browser => {
         browser.end()
     },
-    'Taking a Test': advocate => {
+    'Taking a Test': browser => {
         personalities
-        .waitForElementVisible('@takeTheTest')
-        .allAgreeMax(advocate)
+            .click('@takeTheTest')
+            .allAgreeMax()
+            // .expect.elementVisible('//div[contains(text(), "You often")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "You have a careful")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "When in a group")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "You usually")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "You usually prefer")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "When in a public place,")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "You often drift away into")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "You have got detailed education")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[contains(text(), "Your emotions control")]')
+            // personalities
+            .allAgreeMax()
+            //     .expect.elementVisible('//div[text()="Advocate"]')
+            //     advocate.expect.elementVisible('//div[text()="INFJ-t"]')
+            .pause(3000)
     }
 }
